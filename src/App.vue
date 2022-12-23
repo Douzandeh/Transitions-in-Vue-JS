0da6734a06a1a243e6fc5bc620b0746d5a0a5b01
Hossein <hosseindozandeh799@gmail.com>
2022-12-23 14:24:57 +0330

    added style vue in Home and About

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,73 @@
 
 <template>
   <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
     <router-view v-slot="{ Component }">
-      <transition>
+      <transition name="fade">
         <component :is="Component" />
       </transition>
     </router-view>
   </div>
 </template>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "montserrat", sans-serif;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+}
+
+nav a {
+  margin: 0 0.5rem;
+  color: black;
+  text-decoration: none;
+}
+
+nav a:hover,
+nav a.router-link-exact-active {
+  color: crimson;
+  text-decoration: underline;
+}
+
+.check {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.check label {
+  margin: 0 0.5rem;
+}
+
+main {
+  padding: 1rem;
+  max-width: 768px;
+  margin: 0 auto;
+  will-change: transform, opacity;
+}
+
+h1 {
+  margin-bottom: 1.5rem;
+}
+
+p {
+  margin-bottom: 1rem;
+}
+
+img {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 1.5rem;
+}
+</style>
