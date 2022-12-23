4560293a99307b48b13ef7b1e611cb6a7051e904
Hossein <hosseindozandeh799@gmail.com>
2022-12-23 13:34:12 +0330

    added file

---
+++ src/router/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <img src="https://picsum.photos/800/300" alt="Placehholder" />
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum est eos
+      eius ea fugiat suscipit!
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, iusto
+      maxime possimus asperiores iste voluptate!
+    </p>
+  </main>
+</template>
---
+++ src/router/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, assumenda?
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta nulla
+      libero fugit beatae architecto asperiores! Facere perspiciatis non sed
+      nulla maxime temporibus quisquam velit necessitatibus!
+    </p>
+  </main>
+</template>
