2086f10be13ebd9578376bcc93a96d17e9dca651
Hossein <hosseindozandeh799@gmail.com>
2022-12-23 15:01:09 +0330

    added Slide Slide-down grow-in grow-out

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
+const anim = ref("fade");
+</script>
 <template>
   <div>
     <nav>
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </nav>
+    <div class="check">
+      <label>
+        <input type="radio" name="anim" value="fade" v-model="anim" />Fade
+      </label>
+      <label>
+        <input type="radio" name="anim" value="slide" v-model="anim" />Slide
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="anim"
+          value="slide-down"
+          v-model="anim"
+        />Slide down
+      </label>
+      <label>
+        <input type="radio" name="anim" value="grow-in" v-model="anim" />Grow in
+      </label>
+      <label>
+        <input type="radio" name="anim" value="grow-out" v-model="anim" />Grow
+        out
+      </label>
+    </div>
     <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
+      <transition :name="anim" mode="out-in">
         <component :is="Component" />
       </transition>
     </router-view>
@@ -79,6 +105,46 @@ img {
 
 .fade-enter-active,
 .fade-leave-active {
+  transition: opacity 0.3s ease-out;
+}
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.3s ease-out;
+}
+.slide-down-enter-from,
+.slide-down-leave-to {
+  opacity: 0;
+  transform: translateY(300px);
+}
+
+.slide-down-enter-active,
+.slide-down-leave-active {
+  transition: opacity 0.3s ease-out;
+}
+.grow-in-enter-from,
+.grow-in-leave-to {
+  opacity: 0;
+  transform: scale(0.3);
+}
+
+.grow-in-enter-active,
+.grow-in-leave-active {
+  transition: opacity 0.3s ease-out;
+}
+.grow-out-enter-from,
+.grow-out-leave-to {
+  opacity: 0;
+  transform: scale(1.5);
+}
+
+.grow-out-enter-active,
+.grow-out-leave-active {
   transition: opacity 0.3s ease-out;
 }
 </style>
